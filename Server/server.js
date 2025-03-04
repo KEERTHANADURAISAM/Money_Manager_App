e68de2f8c98a26dd27337b660b8ae4e50288fda3
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-03-05 00:53:19 +0530

    add vercelfile

--- Server/server.js
+++ Server/server.js
@@ -13,16 +13,23 @@ const con = require("./db/connection.js");
 
 app.use(require("./routes/route"));
 
-con
-  .then((db) => {
-    if (!db) return process.exit(1);
+// con
+//   .then((db) => {
+//     if (!db) return process.exit(1);
 
-    const port = 8080;
-    app.listen(port, () => {
-      console.log(`server is running localhost:${port}`);
-    });
-    app.on("error", (err) => console.log(`Failed to conenned ${err}`));
-  })
-  .catch((error) => {
-    console.log(`Connection Failed ${error}`);
-  });
+//     // const port = 8050;
+//     app.listen(port, () => {
+//       console.log(`server is running localhost:${port}`);
+//     });
+//     app.on("error", (err) => console.log(`Failed to conenned ${err}`));
+//   })
+//   .catch((error) => {
+//     console.log(`Connection Failed ${error}`);
+//   });
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To Your Node Server</h1>");
+});
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () =>
+  console.log(`localhost User Server : ${PORT}`)
+);
