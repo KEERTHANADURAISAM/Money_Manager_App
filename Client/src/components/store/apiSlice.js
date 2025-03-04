0eb805b4cbe1f77b353380151f590d4fbf44e39f
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-03-05 01:42:16 +0530

    add env

--- Client/src/components/store/apiSlice.js
+++ Client/src/components/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseURL = "https://money-manager-app-mern.vercel.app/";
+const baseURL =process.env.REACT_APP_API_URL;
 
 
 export const apiSlice = createApi({
