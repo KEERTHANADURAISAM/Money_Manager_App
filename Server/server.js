a85760a72ab6e3709827ea9220e9457102273412
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-03-05 01:20:44 +0530

    new

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const app = express();
-
 const cors = require("cors");
 require("dotenv").config();
 
-const cors = require("cors");
-const port = process.env.PORT;
+const app = express();
 const corsOptions = {
   origin: "*", // Allow all origins (change this for security)
   methods: "GET,POST,PUT,DELETE",
@@ -14,8 +11,30 @@ const corsOptions = {
 
 // const port = process.env.PORT;
 
-app.use(cors());
 app.use(cors(corsOptions));
 app.use(express.json());
 
 const con = require("./db/connection.js");
+
+app.use(require("./routes/route"));
+
+// con
+//   .then((db) => {
+//     if (!db) return process.exit(1);
+
+//     // const port = 8050;
+//     app.listen(port, () => {
+//       console.log(server is running localhost:${port});
+//     });
+//     app.on("error", (err) => console.log(Failed to conenned ${err}));
+//   })
+//   .catch((error) => {
+//     console.log(Connection Failed ${error});
+//   });
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To Your Node Server</h1>");
+});
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () =>
+  console.log(`localhost User Server : ${PORT}`)
+);
